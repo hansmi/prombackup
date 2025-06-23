7d4e4f9096302dfc683eb7cdfbfb981ec7e306c3
Michael Hanselmann <public@hansmi.ch>
2025-06-23 22:59:36 +0200

    Buffer writes to output files to reduce syscalls

--- internal/clientcli/output.go
+++ internal/clientcli/output.go
@@ -1,40 +1,62 @@
 package clientcli
 
 import (
+	"bufio"
 	"io"
 	"os"
 	"path/filepath"
+
+	"go.uber.org/multierr"
 )
+
+const outputBufferSize = 1024 * 1024
 
 type OutputFile struct {
 	path string
 
-	w         io.WriteCloser
-	needClose bool
+	w     io.Writer
+	close func() error
 }
 
 func NewOutputFile(path string) (*OutputFile, error) {
+	f := &OutputFile{}
+
 	switch path {
 	case "":
-		return &OutputFile{}, nil
+		// Use remote filename
 
 	case "-":
-		return &OutputFile{
-			w: os.Stdout,
-		}, nil
+		f.w = os.Stdout
+
+	default:
+		fh, err := os.Create(path)
+		if err != nil {
+			return nil, err
+		}
+
+		f.setup(fh)
 	}
 
-	fh, err := os.Create(path)
-	if err != nil {
-		return nil, err
-	}
-
-	return &OutputFile{
-		w:         fh,
-		needClose: true,
-	}, nil
+	return f, nil
 }
 
+func (f *OutputFile) setup(w io.WriteCloser) {
+	if f.w != nil {
+		return
+	}
+
+	buf := bufio.NewWriterSize(w, outputBufferSize)
+
+	f.w = buf
+	f.close = func() (err error) {
+		defer multierr.AppendInvoke(&err, multierr.Close(w))
+
+		return buf.Flush()
+	}
+}
+
+// Open returns a writer. If a path was given to [NewOutputFile] the hint is
+// unused. Otherwise it's used to create a new file.
 func (f *OutputFile) Open(hint string) (io.Writer, error) {
 	if f.w != nil {
 		return f.w, nil
@@ -47,15 +69,14 @@ func (f *OutputFile) Open(hint string) (io.Writer, error) {
 		return nil, err
 	}
 
-	f.w = fh
-	f.needClose = true
+	f.setup(fh)
 
 	return f.w, nil
 }
 
 func (f *OutputFile) Close() error {
-	if f.w != nil && f.needClose {
-		return f.w.Close()
+	if f.close != nil {
+		return f.close()
 	}
 
 	return nil
